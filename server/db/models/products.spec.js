9e7e4db7222194bf317ee640900fba5adc2ce855
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-09-11 17:00:21 -0400

    product model test complete

--- server/db/models/products.spec.js
+++ server/db/models/products.spec.js
@@ -15,7 +15,7 @@ describe('Product model', () => {
       name: "Milkshakes",
       breed: "spaniel",
       breeder: "Jannine",
-      brederEmail: "j@pew.com",
+      breederEmail: "j@pew.com",
       description: "eiugrh fIOWAHGUIRW ofejhguieroils",
       price: 500,
       photo: "http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg",
@@ -35,32 +35,11 @@ describe('Product model', () => {
         expect(savedPuppy.description).to.equal('eiugrh fIOWAHGUIRW ofejhguieroils')
         expect(savedPuppy.price).to.equal(500)
         expect(savedPuppy.photo).to.equal("http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg")
-        expect(savedPuppy.categories).to.equal(["social", "wet", "beach-friendly"])
+        expect(savedPuppy.categories[0]).to.equal("social")
+        expect(savedPuppy.categories).to.have.lengthOf(3)
         expect(savedPuppy.inventory).to.equal(5)
       })
     })
   })
-  // describe('instanceMethods', () => {
-  //   describe('correctPassword', () => {
-  //     let cody
 
-  //     beforeEach(() => {
-  //       return User.create({
-  //         email: 'cody@puppybook.com',
-  //         password: 'bones'
-  //       })
-  //         .then(user => {
-  //           cody = user
-  //         })
-  //     })
-
-  //     it('returns true if the password is correct', () => {
-  //       expect(cody.correctPassword('bones')).to.be.equal(true)
-  //     })
-
-  //     it('returns false if the password is incorrect', () => {
-  //       expect(cody.correctPassword('bonez')).to.be.equal(false)
-  //     })
-  //   }) // end describe('correctPassword')
-  // }) // end describe('instanceMethods')
 }) // end describe('User model')
