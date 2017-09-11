2ceb98e4e04678f7432bbd021120bd2d0a7a8325
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-09-11 16:43:30 -0400

    Added Testing for Product

--- server/db/models/products.spec.js
+++ server/db/models/products.spec.js
@@ -9,6 +9,37 @@ describe('Product model', () => {
     return db.sync({force: true})
   })
 
+  let puppy;
+  beforeEach(function(){
+    puppy = Products.build({
+      name: "Milkshakes",
+      breed: "spaniel",
+      breeder: "Jannine",
+      brederEmail: "j@pew.com",
+      description: "eiugrh fIOWAHGUIRW ofejhguieroils",
+      price: 500,
+      photo: "http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg",
+      categories: ["social", "wet", "beach-friendly"],
+      inventory: 5
+    });
+  });
+
+  describe('Product', function (){
+    it('includes all the fields', function(){
+      return puppy.save()
+      .then(function(savedPuppy){
+        expect(savedPuppy.name).to.equal('Milkshakes')
+        expect(savedPuppy.breed).to.equal('spaniel')
+        expect(savedPuppy.breeder).to.equal('Jannine')
+        expect(savedPuppy.breederEmail).to.equal('j@pew.com')
+        expect(savedPuppy.description).to.equal('eiugrh fIOWAHGUIRW ofejhguieroils')
+        expect(savedPuppy.price).to.equal(500)
+        expect(savedPuppy.photo).to.equal("http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg")
+        expect(savedPuppy.categories).to.equal(["social", "wet", "beach-friendly"])
+        expect(savedPuppy.inventory).to.equal(5)
+      })
+    })
+  })
   // describe('instanceMethods', () => {
   //   describe('correctPassword', () => {
   //     let cody
