2fc6d025cb1f1b4249c33ff2a9f895e6b7e0d87c
sw-yx <swyx@swyx.io>
2017-09-11 16:39:37 -0400

    done testing on users

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -3,26 +3,47 @@
 const {expect} = require('chai')
 const db = require('../index')
 const User = db.model('user')
+let cody
 
 describe('User model', () => {
   beforeEach(() => {
-    return db.sync({force: true})
+    return db.sync({force: true}).then(() => {
+      return User.create({
+        name: 'Cody the Dog',
+        email: 'cody@puppybook.com',
+        password: 'bones',
+        tags: ['hasOwnedDog', 'City Apartment'],
+        isAdmin: true,
+        googleId: null
+      })
+    })
+    .then(user => {
+          cody = user
+          return cody
+    })
+  })
+
+  describe('attributes definition', function(){
+    it('includes `name` field', function () {
+      expect(cody.name).to.equal('Cody the Dog')
+    })
+    it('includes `email` field', function () {
+      expect(cody.email).to.equal('cody@puppybook.com')
+    })
+    it('includes `isAdmin` field', function () {
+      expect(cody.isAdmin).to.be.true();
+    })
+    it('includes `googleId` field', function () {
+      expect(cody.googleId).to.be.null;
+    })
+    it('includes `tags` field', function () {
+      expect(cody.tags).to.have.length(2);
+      expect(cody.tags[0]).to.equal('hasOwnedDog')
+    })
   })
 
   describe('instanceMethods', () => {
     describe('correctPassword', () => {
-      let cody
-
-      beforeEach(() => {
-        return User.create({
-          email: 'cody@puppybook.com',
-          password: 'bones'
-        })
-          .then(user => {
-            cody = user
-          })
-      })
-
       it('returns true if the password is correct', () => {
         expect(cody.correctPassword('bones')).to.be.equal(true)
       })
