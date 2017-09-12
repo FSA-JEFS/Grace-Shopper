bd589ece77ea7ad2b1f763241011236f4bf7d1e8
sw-yx <swyx@swyx.io>
2017-09-12 15:02:07 -0400

    Refactor and update product route tests

--- server/api/products.spec.js
+++ server/api/products.spec.js
@@ -1,19 +1,18 @@
-const {expect} = require('chai')
-const request = require('supertest')
-const db = require('../db')
-const app = require('../index')
-const Products = db.model('products')
+const { expect } = require("chai");
+const request = require("supertest");
+const db = require("../db");
+const app = require("../index");
+const Products = db.model("products");
 
-describe('Products routes', () => {
+describe("Products routes", () => {
   beforeEach(() => {
-    return db.sync({force: true})
-  })
+    return db.sync({ force: true });
+  });
 
-  describe('/api/products/', () => {
-
+  describe("/api/products/", () => {
     let puppy;
-    let puppy2
-    beforeEach(function(){
+    let puppy2;
+    beforeEach(function() {
       let promise1 = Products.create({
         name: "Milkshakes",
         breed: "spaniel",
@@ -21,10 +20,12 @@ describe('Products routes', () => {
         breederEmail: "j@pew.com",
         description: "eiugrh fIOWAHGUIRW ofejhguieroils",
         price: 500,
-        photos: ["http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg"],
+        photos: [
+          "http://cdn1-www.dogtime.com/assets/uploads/gallery/english-spaniel-dog-breed-pictures/8-fullbody.jpg"
+        ],
         tags: ["social", "wet", "beach-friendly"],
         inventory: 5
-      })
+      });
       let promise2 = Products.create({
         name: "CookiesAndCream",
         breed: "Chowchow",
@@ -32,14 +33,84 @@ describe('Products routes', () => {
         breederEmail: "e@pew.com",
         description: "BEARS!",
         price: 740,
-        photos: ["https://i.pinimg.com/736x/11/12/09/1112097172845da1ab30cf3fc3eed160--white-chow-chow-chow-chow-puppies.jpg"],
+        photos: [
+          "https://i.pinimg.com/736x/11/12/09/1112097172845da1ab30cf3fc3eed160--white-chow-chow-chow-chow-puppies.jpg"
+        ],
         tags: ["fluffy", "adventurous", "beach-friendly"],
         inventory: 1
       });
-      return Promise.all([promise1, promise2])
-      .then(results => {
-        [puppy, puppy2] = results
-      })
+      return Promise.all([promise1, promise2]).then(results => {
+        [puppy, puppy2] = results;
+      });
+    });
+    describe("GET requests: ", () => {
+      it("/api/products", () => {
+        return request(app).get("/api/products").expect(200).then(res => {
+          expect(res.body).to.be.an("array");
+          expect(res.body).to.have.lengthOf(2);
+          expect(res.body[0].name).to.be.equal("Milkshakes");
+        });
+      });
+      it("/api/products/:id", () => {
+        return request(app)
+          .get("/api/products/" + puppy.id)
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an("object");
+            expect(res.body.name).to.be.equal(puppy.name);
+            expect(res.body.breeder).to.be.equal(puppy.breeder);
+          });
+      });
+      it("/api/products/breed/", () => {
+        return request(app)
+          .get("/api/products/breed/spaniel")
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.have.lengthOf(1);
+            expect(res.body[0].name).to.be.equal("Milkshakes");
+          });
+      });
+      it("/api/products/breeder/", () => {
+        return request(app)
+          .get("/api/products/breeder/Jannine")
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.have.lengthOf(1);
+            expect(res.body[0].name).to.be.equal("Milkshakes");
+          });
+      });
+      it("/api/products/price/", () => {
+        return request(app)
+          .get("/api/products/price/600")
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.have.lengthOf(1);
+            expect(res.body[0].name).to.be.equal("Milkshakes");
+          });
+      });
+      it("/api/products/tags/", () => {
+        return request(app)
+          .get("/api/products/tag/fluffy")
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.have.lengthOf(1);
+            expect(res.body[0].name).to.be.equal("CookiesAndCream");
+          });
+      });
+
+      it("/api/products/tags/", () => {
+        return request(app)
+          .get("/api/products/tag/beach-friendly")
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an("array");
+            expect(res.body).to.have.lengthOf(2);
+          });
+      });
     });
 
     it('GET /api/products', () => {
