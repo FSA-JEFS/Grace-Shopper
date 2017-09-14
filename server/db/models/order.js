2afffe7068239ba5fe01a5b4b2e786923492d320
sw-yx <swyx@swyx.io>
2017-09-14 11:22:27 -0400

    edits

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,6 +4,7 @@ const db = require("../db");
 const Order = db.define("order", {
   status: {
     type: Sequelize.ENUM("CREATED", "PROCESSING", "CANCELLED", "COMPLETED"),
+    defaultValue: "CREATED",
     allowNull: false
   },
   items: {
