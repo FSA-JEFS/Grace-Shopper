6308150b836de3866c6791efd2b8e82f82013ff9
jn9cn <jannine.chan@gmail.com>
2017-09-18 10:59:02 -0400

    push before pair up

--- client/components/CheckoutPage.js
+++ client/components/CheckoutPage.js
@@ -2,14 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import {getCart, addToCart, deleteFromCart} from '../store'
+import {createNewOrder} from '../store'
 
 const CheckoutPage = (props) => {
     console.log(">>>>>", props)
+    const {handleSubmit} = props
     const cart = props.cart.product
 
+    // just shove the cart into orders
+
     return (
-        <div className="contact-page">
+        <div className="contact-page" style={{backgroundImage: "url('../resources/assets/img/bg7.jpg')", backgroundSize: 'cover', backgroundPosition: 'top center'}}>
         <div id="contactUsMap" className="big-map"></div>
 
         <div className="main main-raised">
@@ -17,10 +20,62 @@ const CheckoutPage = (props) => {
                 <div className="container">
                     <h2 className="title">Complete Your Order</h2>
                     <div className="row">
-                        <div className="col-md-6">
-                            <p className="description">You can contact us with anything related to our Products. We'll get in touch with you as soon as possible.<br />
+
+                        <div className="col-md-4">
+                            {props.cart.length && props.cart.map((element, index) => (
+                                
+                            <div key={element.id} className="info info-horizontal" >  
+                                <div className="icon icon-primary">
+                                    <div className="material-icons">
+                                        <img src={element.product.photos[0]} alt="..." width="150"/>
+                                    </div>
+                                    <div className="description">
+                                        <h4 className="info-title">{element.product.name}</h4>
+                                        <p> 
+                                            Breeder: {element.product.breeder} <br />
+                                            Breed: {element.product.breed} <br />
+                                            Quantity: {element.quantity} <br />
+                                            Subtotal: ${element.product.price * element.quantity}
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            ))}
+                            
+                            {/* <div className="info info-horizontal">
+                                <div className="icon icon-primary">
+                                    <i className="material-icons">phone</i>
+                                </div>
+                                <div className="description">
+                                    <h4 className="info-title">Give us a ring</h4>
+                                    <p> Michael Jordan<br />
+                                        +40 762 321 762<br />
+                                        Mon - Fri, 8:00-22:00
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="info info-horizontal">
+                                <div className="icon icon-primary">
+                                    <i className="material-icons">business_center</i>
+                                </div>
+                                <div className="description">
+                                    <h4 className="info-title">Legal Information</h4>
+                                    <p> Creative Tim Ltd.<br />
+                                        VAT &middot; EN2341241<br />
+                                        IBAN &middot; EN8732ENGB2300099123<br />
+                                        Bank &middot; Great Britain Bank
+                                    </p>
+                                </div>
+                            </div> */}
+
+                        </div>
+
+                        <div className="col-md-6 col-md-offset-2">
+                            <p className="description">Enter your details below! We'll deliver your puppies right away.<br />
                             </p>
-                            <form role="form" id="contact-form" method="post">
+                            <form onSubmit={handleSubmit} role="form" id="contact-form" method="post">
                                 <div className="form-group label-floating">
                                     <label className="control-label">Your name</label>
                                     <input type="text" name="name" className="form-control" />
@@ -33,7 +88,7 @@ const CheckoutPage = (props) => {
                                 <br />
                                 <div className="form-group label-floating">
                                     <label className="control-label">Shipping Address</label>
-                                    <input type="text" name="phone" className="form-control"/>
+                                    <input type="text" name="address" className="form-control"/>
                                 </div>
                                 <div className="form-group label-floating">
                                     <label className="control-label">Phone</label>
@@ -50,51 +105,29 @@ const CheckoutPage = (props) => {
                                 </div>
                             </form>
                         </div>
-                        <div className="col-md-4 col-md-offset-2">
-                            <div className="info info-horizontal">
-                                <div className="icon icon-primary">
-                                    <i className="material-icons">pin_drop</i>
-                                </div>
-                                <div className="description">
-                                    <h4 className="info-title">Find us at the office</h4>
-                                    <p> Bld Mihail Kogalniceanu, nr. 8,<br />
-                                        7652 Bucharest,<br />
-                                        Romania
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="info info-horizontal">
-                                <div className="icon icon-primary">
-                                    <i className="material-icons">phone</i>
-                                </div>
-                                <div className="description">
-                                    <h4 className="info-title">Give us a ring</h4>
-                                    <p> Michael Jordan<br />
-                                        +40 762 321 762<br />
-                                        Mon - Fri, 8:00-22:00
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="info info-horizontal">
-                                <div className="icon icon-primary">
-                                    <i className="material-icons">business_center</i>
-                                </div>
-                                <div className="description">
-                                    <h4 className="info-title">Legal Information</h4>
-                                    <p> Creative Tim Ltd.<br />
-                                        VAT &middot; EN2341241<br />
-                                        IBAN &middot; EN8732ENGB2300099123<br />
-                                        Bank &middot; Great Britain Bank
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-                </div>
+                        
+                    </div>
+            </div>
             </div>
         </div>
         </div>
       )
+    }
+
+    const mapDispatch = (dispatch) => {
+        return {
+            handleSubmit (evt) {
+                evt.preventDefault()
+                // submit cart data into the user's orders
+                // create an obj 
+                let order = {
+                    status: 'CREATED', 
+                    items: [].push(props.cart.map((element, index) => {element.product.name})), 
+                    subTotal: props.cart.map((element, index) => {element.product.price * element.quantity}) 
+                }
+                dispatch(makeNewOrder(order))
+            }
+        }
     }
     
     const mapPropToCart = (state) => {
@@ -105,21 +138,6 @@ const CheckoutPage = (props) => {
         //   console.log(word)
         //   addToCart(word)
         // }
-      }
-    }
-    
-    const mapDispatch = (dispatch) => {
-      return {
-        handlDelete(index){
-            //dispatch(deleteFromCart(index))
-        },
-        handlePlus(product){
-            dispatch(addToCart(product))
-        },
-        handleMinus(product){
-            console.log("*****", product)
-            dispatch(deleteFromCart(product))
-        }
       }
     }
     
